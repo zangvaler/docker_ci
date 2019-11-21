be6e245a776ea0cf5f52d861bd720e411423c862
zangval <web_zangval@163.com>
2019-11-21 15:06:47 +0800

    update commit

--- webhooks.js
+++ webhooks.js
@@ -1,6 +1,6 @@
 var http = require('http')
 var createHandler = require('github-webhook-handler')
-var handler = createHandler({ path: '/webhooks', secret: 'myHashSecret' })
+var handler = createHandler({ path: '/webhooks', secret: 'myhashSecret' })
 // 上面的 secret 保持和 GitHub 后台设置的一致
 
 function run_cmd(cmd, args, callback) {
